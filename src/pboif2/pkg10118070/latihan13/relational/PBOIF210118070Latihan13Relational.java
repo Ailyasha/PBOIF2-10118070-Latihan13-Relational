038040e06a820f3cd6c834793bbe75cdc4e35ddb
Aufa Ilyasha <55798997+Ailyasha@users.noreply.github.com>
2019-09-25 22:41:39 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118070/latihan13/relational/PBOIF210118070Latihan13Relational.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118070.latihan13.relational;
+
+/**
+ *
+ * @author
+ * NAMA     : Aufa Ilyasha
+ * Kelas    : IF2
+ * NIM      : 10118070
+ */
+public class PBOIF210118070Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+    }
+    
+}
